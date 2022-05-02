d9ce7381d32412862a63fa0f9142baa928503b10
hotrainy <113290152+hotrainy@users.noreply.github.com>
2022-05-02 06:39:23 +0000

    Menu fixes

--- config/helpers.js
+++ config/helpers.js
@@ -50,4 +50,9 @@ export const silentExit = (code = 0) => {
 };
 
 // Set the console colours
-console.orange = msg 
+console.orange = msg => console.log('\x1b[33m%s\x1b[0m', msg);
+console.green = msg => console.log('\x1b[32m%s\x1b[0m', msg);
+console.red = msg => console.log('\x1b[31m%s\x1b[0m', msg);
+console.blue = msg => console.log('\x1b[34m%s\x1b[0m', msg);
+console.purple = msg => console.log('\x1b[35m%s\x1b[0m', msg);
+console.cyan = msg => console.log('\x1b[36m%s\x1b[0m', 
