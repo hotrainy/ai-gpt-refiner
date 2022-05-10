4dbc41cf166d85535c36267bfcecc7e64b2ae4ce
hotrainy <113290152+hotrainy@users.noreply.github.com>
2022-05-10 05:39:06 +0000

    by ice

--- config/update.js
+++ config/update.js
@@ -2,4 +2,19 @@ import { execSync } from 'child_process';
 import { deleteNodeModules, getRootDir } from './helpers.js';
 
 const config = {
-    skipGit: proc
+    skipGit: process.argv.includes('-g'),
+};
+
+// Set the directories
+const rootDir = getRootDir();
+const directories = [rootDir];
+
+(async () => {
+    console.green(
+        'Starting update script, this may take a minute or two depending on your system and network.',
+    );
+
+    const { skipGit } = config;
+    if (!skipGit) {
+    // Fetch latest repo
+        console.p
