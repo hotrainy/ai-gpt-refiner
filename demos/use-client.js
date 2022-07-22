e22e5671540ba3d39f579fccc23a3a1f03638088
hotrainy <113290152+hotrainy@users.noreply.github.com>
2022-07-22 19:52:03 +0000

    Update submodule references

--- demos/use-client.js
+++ demos/use-client.js
@@ -4,4 +4,8 @@ import { KeyvFile } from 'keyv-file';
 import { ChatGPTClient } from '../index.js';
 
 const clientOptions = {
-    // (Optional) Support for a reverse proxy for the completions endpoint (private API server)
+    // (Optional) Support for a reverse proxy for the completions endpoint (private API server).
+    // Warning: This will expose your `openaiApiKey` to a third party. Consider the risks before using this.
+    // reverseProxyUrl: 'https://chatgpt.hato.ai/completions',
+    // (Optional) Parameters as described in https://platform.openai.com/docs/api-reference/completions
+    // (Optional) to use Azure OpenAI API, set `azure` to
