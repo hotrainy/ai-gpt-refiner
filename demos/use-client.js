0cf6ece0e64583775b13eff6e7bd0fecc235dfa6
hotrainy <113290152+hotrainy@users.noreply.github.com>
2022-07-23 21:47:07 +0000

    NEWS updates

--- demos/use-client.js
+++ demos/use-client.js
@@ -57,4 +57,11 @@ response = await chatGptClient.sendMessage('Now write it in French.', {
     parentMessageId: response.messageId,
     // If you want streamed responses, you can set the `onProgress` callback to receive the response as it's generated.
     // You will receive one token at a time, so you will need to concatenate them yourself.
-   
+    onProgress: token => process.stdout.write(token),
+});
+console.log();
+console.log(response.response); // Doux et élégant, avec des yeux qui brillent,\nLes chats sont des créatures de grâce suprême.\n...
+
+response = await chatGptClient.sendMessage('Repeat my 2nd message verbatim.', {
+    conversationId: response.conversationId,
+    par
