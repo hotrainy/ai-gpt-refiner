aa805a3c8b1e0a7bee4d5755b71924d62558fff7
hotrainy <113290152+hotrainy@users.noreply.github.com>
2022-10-07 07:22:03 +0000

    Update fluxion

--- src/BingAIClient.js
+++ src/BingAIClient.js
@@ -545,4 +545,10 @@ export default class BingAIClient {
                             message: eventMessage,
                             conversationExpiryTime: event?.item?.conversationExpiryTime,
                         });
-                        // eslint-disa
+                        // eslint-disable-next-line no-useless-return
+                        return;
+                    }
+                    case 7: {
+                        // [{"type":7,"error":"Connection closed with an error.","allowReconnect":true}]
+                        clearTimeout(messageTimeout);
+                        this.constructor.cleanupWebSocketConnecti
