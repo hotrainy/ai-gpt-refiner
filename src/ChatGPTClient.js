d1d99cca27249e52d7f243b4e88c6442dd0a6d95
hotrainy <113290152+hotrainy@users.noreply.github.com>
2022-10-19 16:54:24 +0000

    deletes

--- src/ChatGPTClient.js
+++ src/ChatGPTClient.js
@@ -2,4 +2,19 @@ import './fetch-polyfill.js';
 import crypto from 'crypto';
 import Keyv from 'keyv';
 import { encoding_for_model as encodingForModel, get_encoding as getEncoding } from '@dqbd/tiktoken';
-import { fetchEventS
+import { fetchEventSource } from '@waylaidwanderer/fetch-event-source';
+import { Agent, ProxyAgent } from 'undici';
+
+const CHATGPT_MODEL = 'gpt-3.5-turbo';
+
+const tokenizersCache = {};
+
+export default class ChatGPTClient {
+    constructor(
+        apiKey,
+        options = {},
+        cacheOptions = {},
+    ) {
+        this.apiKey = apiKey;
+
+        cacheOptions.namespace
